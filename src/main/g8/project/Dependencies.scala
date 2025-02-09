29548e5c4fb767807f7e747776c8dbdd0ee569e1
Watson Dinh <mr.kurro@gmail.com>
2025-02-09 09:28:48 -0600

    catzio interopt dep

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,12 +2,10 @@ object Dependencies {
   val Tyrian    = "0.12.0"
   val Scalatest = "3.2.15"
   val Quicklens = "1.9.7"
-
-  $if(use_zio.truthy) $
+  val JavaSecureRandom = "1.0.0"
+  $if(use_zio.truthy || backend.truthy) $
   val ZioInteropCats = "23.1.0.3"
   $endif$
-  val JavaSecureRandom = "1.0.0"
-
   $if(backend.truthy) $
   val ZioJson        = "0.7.8"
   val ZioHttp        = "3.0.1"
