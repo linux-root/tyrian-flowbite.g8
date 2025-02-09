514785275f703e0401490e5e7b25dcbe89d3879b
Watson Dinh <mr.kurro@gmail.com>
2025-02-09 02:45:01 -0600

    add truthy

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,12 +3,12 @@ object Dependencies {
   val Scalatest = "3.2.15"
   val Quicklens = "1.9.7"
 
-  $if(use_zio) $
-  val ZioInteropCats = "23.1.0.3"
+  $if(use_zio.truthy) $
+  val ZioInteropCats = "23.1.0.3"j
   $endif$
   val JavaSecureRandom = "1.0.0"
 
-  $if(backend) $
+  $if(backend.truthy) $
   val ZioJson        = "0.7.8"
   val ZioHttp        = "3.0.1"
   val JwtScala       = "10.0.1"
