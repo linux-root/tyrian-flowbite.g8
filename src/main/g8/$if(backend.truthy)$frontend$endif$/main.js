d912e35b5c1b6875f2d15235a6c04bf832cbc5f6
Watson Dinh <ping@w47s0n.com>
2025-06-08 16:42:57 -0500

    update main.js

--- src/main/g8/$if(backend.truthy)$frontend$endif$/main.js
+++ src/main/g8/$if(backend.truthy)$frontend$endif$/main.js
@@ -1,7 +1,2 @@
+import 'scalajs'
 import 'resources/index.css'
-
-if (import.meta.env.DEV) {
-  import('./target/scala-$scala_version$/frontend-fastopt/main.js');
-} else {
-  import('./target/scala-$scala_version$/frontend-opt/main.js');
-}
--- src/main/g8/$if(backend.truthy)$frontend$endif$/vite.config.js
+++ src/main/g8/$if(backend.truthy)$frontend$endif$/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig(({ command }) => {
   return {
     resolve: {
       alias: {
+        "scalajs": command === "serve" ? './target/scala-$scala_version$/frontend-fastopt/main.js' : './target/scala-$scala_version$/frontend-opt/main.js',
         "resources": path.resolve(__dirname, "./src/main/resources"),
         "js": path.resolve(__dirname, "./src/main/js"),
       }
