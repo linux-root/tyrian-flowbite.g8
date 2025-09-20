94018352a85809ed7708f9cd2068b05014fcacd6
Watson Dinh <ping@w47s0n.com>
2025-09-20 16:39:36 -0500

    update scala version

--- src/main/g8/$if(backend.truthy)$frontend$endif$/vite.config.js
+++ src/main/g8/$if(backend.truthy)$frontend$endif$/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig(({ command }) => {
+  const scalaVersion = '$scala_version$'
   return {
     resolve: {
       alias: {
-        "scalajs": command === "serve" ? './target/scala-$scala_version$/frontend-fastopt/main.js' : './target/scala-$scala_version$/frontend-opt/main.js',
+        "scalajs": command === "serve" ? `./target/scala-\${scalaVersion}/frontend-fastopt/main.js` : `./target/scala-\${scalaVersion}/frontend-opt/main.js`,
         "resources": path.resolve(__dirname, "./src/main/resources"),
         "js": path.resolve(__dirname, "./src/main/js"),
       }
