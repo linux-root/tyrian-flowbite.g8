9053a4f619e7b4a8a7c608b09b2bd730ab4bf86d
Watson Dinh <ping@w47s0n.com>
2026-09-09 22:05:42 -0500

    tailwindcss v4

--- src/main/g8/$if(backend.truthy)$frontend$endif$/vite.config.js
+++ src/main/g8/$if(backend.truthy)$frontend$endif$/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command }) => {
   const scalaVersion = '$scala_version$'
   return {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         "scalajs": command === "serve" ? `./target/scala-\${scalaVersion}/frontend-fastopt/main.js` : `./target/scala-\${scalaVersion}/frontend-opt/main.js`,
